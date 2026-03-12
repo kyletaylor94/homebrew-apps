3351ecdf36efc2c6496c89c62c8a1598dc9338e2
T. Csaba <113541369+kyletaylor94@users.noreply.github.com>
2026-03-12 21:52:29 +0100

    Update CoreDataBrowser to version 1.0.1

--- Casks/coredatabrowser.rb
+++ Casks/coredatabrowser.rb
@@ -1,6 +1,6 @@
 cask "coredatabrowser" do
-  version "1.0.0"
-  sha256 "bf52f112a4a6579e9b0a6b1d2a92209b19856de712aaecdfc54ffd5768e8b0ff"
+  version "1.0.1"
+  sha256 "f6a2faf625034487f1956dfdf8ccf33e6b8263e1400f8db53e0f3ac6fe38550b"
 
   url "https://github.com/kyletaylor94/CoreDataBrowser/releases/download/v#{version}/CoreDataBrowser.zip"
   name "CoreDataBrowser"
